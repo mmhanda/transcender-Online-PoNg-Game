25c4439f2d405184d8ebdff140c090ae3b0f190e
mohammed <mohammedhanda109@gmail.com>
2023-10-14 16:37:14 +0100

    collision problem fixed ball was stuck on the paddle

--- logic.js
+++ logic.js
@@ -1,31 +1,33 @@
 import Ball from "./Ball.js";
 import Paddle from "./Paddle.js";
 
-const ball = new Ball(document.getElementById('ball'));
-const playerPaddle = new Paddle(document.getElementById('player-paddle'));
-const botPaddle = new Paddle(document.getElementById('bot-paddle'));
-const playerScoreElem = document.getElementById('player-score');
+const ball = new Ball(document.getElementById("ball"));
+const playerPaddle = new Paddle(document.getElementById("player-paddle"));
+const botPaddle = new Paddle(document.getElementById("bot-paddle"));
+const playerScoreElem = document.getElementById("player-score");
 const botScoreElem = document.getElementById("bot-score");
 
 let LastTime = null;
 
-function update (time) {
-  
+function update(time) {
   if (LastTime != null) {
     const delta = time - LastTime;
     ball.update(delta, [playerPaddle.rect(), botPaddle.rect()]);
     botPaddle.update(delta, ball.y);
-    if (isLose()) { handleLose() }
-    
-    const hue = parseFloat(getComputedStyle(document.documentElement)
-                  .getPropertyValue("--hue"));
-    document.documentElement.style.setProperty("--hue", hue + delta * 0.04);  
+    if (isLose()) {
+      handleLose();
+    }
+
+    const hue = parseFloat(
+      getComputedStyle(document.documentElement).getPropertyValue("--hue")
+    );
+    document.documentElement.style.setProperty("--hue", hue + delta * 0.04);
   }
   LastTime = time;
   window.requestAnimationFrame(update);
 }
 
-function handleLose () {
+function handleLose() {
   const rect = ball.rect();
 
   if (rect.right >= window.innerWidth) {
@@ -42,9 +44,8 @@ function isLose() {
   return rect.left <= 0 || rect.right >= window.innerWidth;
 }
 
-document.addEventListener("mousemove", e => {
+document.addEventListener("mousemove", (e) => {
   playerPaddle.position = (e.y / window.innerHeight) * 100; //for getting to percentenge of the window of where the mouse is and the paddle should go
-})
-
+});
 
 window.requestAnimationFrame(update);
