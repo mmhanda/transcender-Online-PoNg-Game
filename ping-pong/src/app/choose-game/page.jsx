891e66b67d36013c7baedfecbb862f0aa060c162
mohammed <mohammedhanda109@gmail.com>
2023-11-11 07:59:09 +0100

    landing page

--- ping-pong/src/app/choose-game/page.jsx
+++ ping-pong/src/app/choose-game/page.jsx
@@ -1,11 +1,25 @@
+"use client";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
 const Page = () => {
+  const [backgroundColor, setBackgroundColor] = useState("bg-gray-700");
+
+  useEffect(() => {
+    setBackgroundColor("bg-gray-700");
+    return () => setBackgroundColor("bg-gray-700");
+  }, []);
+
   return (
-    <div className="flex justify-around items-center h-screen bg-gray-700">
+    <div
+      className={`flex justify-around items-center h-screen ${backgroundColor}`}
+    >
       <Link href="/game-bot" passHref>
-        <div className="text-center cursor-pointer mx-4 transform transition-transform hover:scale-105">
+        <div
+          className="text-center cursor-pointer mx-4 transform transition-transform hover:scale-105"
+          onClick={() => setBackgroundColor("bg-gray-700")}
+        >
           <Image
             src="/choose-game-assets/ai.jpg"
             alt="AI"
@@ -17,7 +31,10 @@ const Page = () => {
       </Link>
 
       <Link href="/game" passHref>
-        <div className="text-center cursor-pointer mx-4 transform transition-transform hover:scale-105">
+        <div
+          className="text-center cursor-pointer mx-4 transform transition-transform hover:scale-105"
+          onClick={() => setBackgroundColor("bg-gray-700")}
+        >
           <Image
             src="/choose-game-assets/online.jpg"
             alt="Online"
@@ -29,14 +46,17 @@ const Page = () => {
       </Link>
 
       <Link href="/game-local" passHref>
-        <div className="text-center cursor-pointer mx-4 transform transition-transform hover:scale-105">
+        <div
+          className="text-center cursor-pointer mx-4 transform transition-transform hover:scale-105"
+          onClick={() => setBackgroundColor("bg-gray-700")}
+        >
           <Image
             src="/choose-game-assets/offline.jpg"
             alt="Offline"
             width={500}
             height={500}
           />
-          <p className="mt-2 text-lg font-bold">Play offline</p>
+          <p className="mt-2 text-lg font-bold">Play locally</p>
         </div>
       </Link>
     </div>
